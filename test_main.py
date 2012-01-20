b6a6e4c082056d8d6eed112b6da4442f0f654ac4
Juan Pedro Bolivar Puente <raskolnikov@gnu.org>
2012-01-20 21:37:12 +0100

    Initial commit
    Files extracted and modified from:
         https://github.com/arximboldi/jpblib
         https://code.launchpad.net/~raskolnikov/+junk/jpblib

---
+++ test_main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+#  File:       test_main.py
+#  Author:     Juan Pedro Bolívar Puente <raskolnikov@es.gnu.org>
+#  Date:       Fri Jan 20 16:29:21 2012
+#  Time-stamp: <2015-07-21 17:36:43 jbo>
+#
+
+#
+#  Copyright (C) 2012 Juan Pedro Bolívar Puente
+#
+#  This file is part of jpblib.
+#
+#  jpblib is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  jpblib is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Unit test runner.
+"""
+
+import unittest
+
+if __name__ == '__main__':
+    unittest.main ()
