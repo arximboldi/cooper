e02eecbf3dd96b55fc5e9ed5fd309ff367ccec69
jbo <jbo@ableton.com>
2015-07-22 15:59:40 +0200

    Add setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='cooper',
+    version='0.1.0',
+    description='Easy and safe cooperative methods',
+    author='Juan Pedro Bolívar Puente',
+    author_email='raskolnikov@gnu.org',
+    url='https://github.com/arximboldi/cooper',
+    download_url = 'https://github.com/arximboldi/cooper/tarball/0.1.0',
+    packages=['cooper']
+)
