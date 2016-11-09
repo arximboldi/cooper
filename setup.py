496cbb3bacf605422b909864ee0b321af6dabd9d
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-11-09 18:11:20 +0100

    Bump version 0.1.3

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
     name='cooper',
-    version='0.1.2',
+    version='0.1.3',
     description='Easy and safe cooperative methods',
     long_description=open('README.rst').read(),
     author='Juan Pedro Bolívar Puente',
     author_email='raskolnikov@gnu.org',
     url='https://github.com/arximboldi/cooper',
-    download_url='https://github.com/arximboldi/cooper/tarball/0.1.2',
+    download_url = 'https://github.com/arximboldi/cooper/tarball/0.1.3',
     packages=['cooper']
 )
